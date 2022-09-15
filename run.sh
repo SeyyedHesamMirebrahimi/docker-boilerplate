c350a11fedb0b3b3dcacc75c1e11f51f06428274
H.Mirebrahimi <hesaam.mir@gmail.com>
2022-09-15 16:34:43 +0430

    init

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+APP_USERNAME=${1}
+PMA_PORT=${2}
+APP_PORT=${3}
+rm -rf docker-compose.yml
+cat docker-compose.sample | sed -e 's/${APP_USERNAME}/'${APP_USERNAME}'/' | sed -e 's/${HOST_PORT}/'${APP_PORT}'/' | sed -e 's/${PMA_PORT}/'${PMA_PORT}'/' >> docker-compose.yml
+create_network="docker network create network_"$APP_USERNAME
+echo $create_network
+$create_network
+run="docker-compose up -d"
+echo $run
+$run
